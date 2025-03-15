23a503f50bcba0962d9d2d50eb4539be843555a4
Evandita <evanditaw@gmail.com>
2025-03-15 22:32:30 +0700

    Added Login and Logout Functionality in FE

--- src/DecentraLearn_backend/src/lib.rs
+++ src/DecentraLearn_backend/src/lib.rs
@@ -1,4 +1,13 @@
+use candid::Principal;
+
 #[ic_cdk::query]
 fn greet(name: String) -> String {
     format!("Hello, {}!", name)
 }
+
+#[ic_cdk::query]
+fn whoami() -> Principal {
+    ic_cdk::caller()
+}
+
+ic_cdk::export_candid!();
