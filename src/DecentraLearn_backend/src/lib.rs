3a3271517e3ad8b840e71ee1c0853be8747f4dea
kyomoto <coolcmyk@github.com>
2025-03-27 13:54:08 +0700

    add untested ai function, test pls

--- src/DecentraLearn_backend/src/lib.rs
+++ src/DecentraLearn_backend/src/lib.rs
@@ -5,6 +5,10 @@ use candid::Principal;
 use models::course::*;
 use controllers::course::*;
 
+use ic_cdk::update;
+use ic_llm::{ChatMessage, Model};
+
+
 #[ic_cdk::query]
 fn greet(name: String) -> String {
     format!("Hello, {}!", name)
@@ -13,6 +17,16 @@ fn greet(name: String) -> String {
 #[ic_cdk::query]
 fn whoami() -> Principal {
     ic_cdk::caller()
+}
+
+#[update]
+async fn prompt(prompt_str: String) -> String {
+    ic_llm::prompt(Model::Llama3_1_8B, prompt_str).await
+}
+
+#[update]
+async fn chat(messages: Vec<ChatMessage>) -> String {
+    ic_llm::chat(Model::Llama3_1_8B, messages).await
 }
 
 // Export candid interface
